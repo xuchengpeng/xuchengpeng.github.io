5060958565e27676b570b53d65659cbe72a9e9e2
xuchengpeng <chuckxcp@qq.com>
2025-04-23 09:52:20 +0800

    add search

---
+++ themes/x/assets/js/search.js
@@ -0,0 +1,111 @@
+var fuse;
+var searchInput = document.getElementById("search-input");
+var searchResults = document.getElementById("search-results");
+var first = searchResults.firstChild;
+var last = searchResults.lastChild;
+var hasResults = false;
+
+// load index
+window.addEventListener("load", (event) => {
+  let xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        let data = JSON.parse(xhr.responseText);
+        if (data) {
+          // fuse.js options; check fuse.js website for details
+          let options = {
+            shouldSort: true,
+            ignoreLocation: true,
+            threshold: 0.0,
+            includeMatches: true,
+            keys: [
+              'title',
+              'permalink',
+              'summary',
+              'content'
+            ]
+          };
+          fuse = new Fuse(data, options); // build the index from the json file
+        }
+      } else {
+        console.log(xhr.responseText);
+      }
+    }
+  };
+  xhr.open('GET', "../index.json");
+  xhr.send();
+});
+
+// execute search as each character is typed
+searchInput.onkeyup = function (event) {
+  if (fuse) {
+    let results = fuse.search(this.value);
+    if (results.length !== 0) {
+      let resultSet = "";
+      for (let item in results) {
+        resultSet += `<li><a href="${results[item].item.permalink}" aria-label="${results[item].item.title}"></a>
+          <h1>${results[item].item.title}</h1>
+          <p>${results[item].item.summary}</p></li>`
+      }
+      searchResults.innerHTML = resultSet;
+      hasResults = true;
+      first = searchResults.firstChild.firstElementChild;
+      last = searchResults.lastChild.firstElementChild;
+    } else {
+      searchResults.innerHTML = "";
+      hasResults = false;
+    }
+  }
+};
+
+function clearSearch() {
+  hasResults = false;
+  searchResults.innerHTML = "";
+  searchInput.value = "";
+  searchInput.focus();
+}
+
+searchInput.addEventListener("search", (event) => {
+  // clicked on x
+  if (!this.value) {
+    clearSearch();
+  }
+});
+
+document.addEventListener("keydown", function (event) {
+  if (event.key == "Escape") {
+    clearSearch();
+  } else if (event.key == "ArrowDown") {
+    if (hasResults) {
+      event.preventDefault();
+      if (document.activeElement == searchInput) {
+        first.focus();
+      } else if (document.activeElement == last) {
+        last.focus();
+      } else {
+        document.activeElement.parentElement.nextSibling.firstElementChild.focus();
+      }
+    }
+  } else if (event.key == "ArrowUp") {
+    if (hasResults) {
+      event.preventDefault();
+      if (document.activeElement == searchInput) {
+        searchInput.focus();
+      } else if (document.activeElement == first) {
+        searchInput.focus();
+      } else {
+        document.activeElement.parentElement.previousSibling.firstElementChild.focus();
+      }
+    }
+  } else if (event.key == "Enter") {
+    if (hasResults) {
+      event.preventDefault();
+      if (document.activeElement == searchInput) {
+        first.focus();
+      } else {
+        document.activeElement.click();
+      }
+    }
+  }
+});
