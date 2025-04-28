db8d36c5eca42fd277996c53c169d268a91aa609
xuchengpeng <chuckxcp@qq.com>
2025-04-28 18:41:06 +0800

    simply toggle menu

--- themes/x/assets/js/menu.js
+++ themes/x/assets/js/menu.js
@@ -1,13 +1,6 @@
 var menuButton = document.getElementById("menu-button");
 var mobileMenu = document.getElementById("mobile-menu");
-var menuOpen = false;
 
 menuButton.addEventListener("click", () => {
-  if (!menuOpen) {
-    menuOpen = true;
-    mobileMenu.style.display = "block";
-  } else {
-    menuOpen = false;
-    mobileMenu.style.display = "none";
-  }
+  mobileMenu.style.display = mobileMenu.style.display === "block" ? "none" : "block";
 });
