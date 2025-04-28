5d2aaff715fdefecb050aa1ca9336171693508ed
xuchengpeng <chuckxcp@qq.com>
2025-04-28 09:33:36 +0800

    add copy code button

---
+++ themes/x/assets/js/code.js
@@ -0,0 +1,22 @@
+async function copyCodeToClipboard(button, highlightDiv) {
+  const codeToCopy = highlightDiv.querySelector(":last-child").textContent;
+  await navigator.clipboard.writeText(codeToCopy);
+  button.innerText = "Copied";
+  setTimeout(function () {
+    button.innerText = "Copy";
+  }, 2000);
+}
+
+function createCopyButton(highlightDiv) {
+  const button = document.createElement("button");
+  button.className = "copy-code";
+  button.type = "button";
+  button.ariaLabel = "Copy";
+  button.innerText = "Copy";
+  button.addEventListener("click", () => copyCodeToClipboard(button, highlightDiv));
+  highlightDiv.appendChild(button);
+}
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  document.querySelectorAll(".highlight").forEach((highlightDiv) => createCopyButton(highlightDiv));
+});
