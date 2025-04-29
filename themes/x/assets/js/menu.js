2a61ebe6c6dc9b87aeb30c91df595a752f5915b8
xuchengpeng <chuckxcp@qq.com>
2025-04-29 09:35:29 +0800

    improve menu toggle

--- themes/x/assets/js/menu.js
+++ themes/x/assets/js/menu.js
@@ -2,5 +2,5 @@ var menuButton = document.getElementById("menu-button");
 var menuList = document.getElementById("menu-list");
 
 menuButton.addEventListener("click", () => {
-  menuList.classList.toggle("block");
+  menuList.style.display = (getComputedStyle(menuList).display === "none") ? "block" : "none";
 });
